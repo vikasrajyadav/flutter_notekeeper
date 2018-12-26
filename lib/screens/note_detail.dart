55587623bc51897a55e1e48d51e441762b97bb52
VikasrajYadav <vry420@gmail.com>
2018-12-27 03:40:08 +0530

    Major changes in code and CRUD opreation

--- lib/screens/note_detail.dart
+++ lib/screens/note_detail.dart
@@ -1,49 +1,64 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:notekeeper/models/note.dart';
+import 'package:notekeeper/utils/database_helper.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:intl/intl.dart';
+
 
 class NoteDetail extends StatefulWidget {
 
-String appBarTitle;
+	final String appBarTitle;
+	final Note note;
 
-NoteDetail(this.appBarTitle);
+	NoteDetail(this. note, this.appBarTitle);
 
 	@override
   State<StatefulWidget> createState() {
 
-    return NoteDetailState(this.appBarTitle);
+    return NoteDetailState(this.note, this.appBarTitle);
   }
 }
 
 class NoteDetailState extends State<NoteDetail> {
 
-  String appBarTitle;
 	static var _priorities = ['High', 'Low'];
+
+	DatabaseHelper helper = DatabaseHelper();
+
+	String appBarTitle;
+	Note note;
 
 	TextEditingController titleController = TextEditingController();
 	TextEditingController descriptionController = TextEditingController();
 
-  NoteDetailState(this.appBarTitle);
+	NoteDetailState(this.note, this.appBarTitle);
 
 	@override
   Widget build(BuildContext context) {
 
 		TextStyle textStyle = Theme.of(context).textTheme.title;
 
+		titleController.text = note.title;
+		descriptionController.text = note.description;
+
     return WillPopScope(
 
-      onWillPop: (){
-        moveToLastScreen();
-      },
+	    onWillPop: () {
+	    	// Write some code to control things, when user press Back navigation button in device navigationBar
+		    moveToLastScreen();
+	    },
 
-    child: Scaffold(
+	    child: Scaffold(
 	    appBar: AppBar(
 		    title: Text(appBarTitle),
-        leading: IconButton(icon: Icon(
-          Icons.arrow_back
-        ),
-        onPressed: (){
-          moveToLastScreen();
-        }
-        ,),
+		    leading: IconButton(icon: Icon(
+				    Icons.arrow_back),
+				    onPressed: () {
+		    	    // Write some code to control things, when user press back button in AppBar
+		    	    moveToLastScreen();
+				    }
+		    ),
 	    ),
 
 	    body: Padding(
@@ -63,11 +78,12 @@ class NoteDetailState extends State<NoteDetail> {
 
 							    style: textStyle,
 
-							    value: 'Low',
+							    value: getPriorityAsString(note.priority),
 
 							    onChanged: (valueSelectedByUser) {
 							    	setState(() {
 							    	  debugPrint('User selected $valueSelectedByUser');
+							    	  updatePriorityAsInt(valueSelectedByUser);
 							    	});
 							    }
 					    ),
@@ -81,6 +97,7 @@ class NoteDetailState extends State<NoteDetail> {
 						    style: textStyle,
 						    onChanged: (value) {
 						    	debugPrint('Something changed in Title Text Field');
+						    	updateTitle();
 						    },
 						    decoration: InputDecoration(
 							    labelText: 'Title',
@@ -100,6 +117,7 @@ class NoteDetailState extends State<NoteDetail> {
 						    style: textStyle,
 						    onChanged: (value) {
 							    debugPrint('Something changed in Description Text Field');
+							    updateDescription();
 						    },
 						    decoration: InputDecoration(
 								    labelText: 'Description',
@@ -127,6 +145,7 @@ class NoteDetailState extends State<NoteDetail> {
 									    onPressed: () {
 									    	setState(() {
 									    	  debugPrint("Save button clicked");
+									    	  _save();
 									    	});
 									    },
 								    ),
@@ -145,6 +164,7 @@ class NoteDetailState extends State<NoteDetail> {
 									    onPressed: () {
 										    setState(() {
 											    debugPrint("Delete button clicked");
+											    _delete();
 										    });
 									    },
 								    ),
@@ -158,11 +178,100 @@ class NoteDetailState extends State<NoteDetail> {
 		    ),
 	    ),
 
-    ),);
+    ));
   }
 
-void moveToLastScreen(){
-  Navigator.pop(context);
-}
+  void moveToLastScreen() {
+		Navigator.pop(context, true);
+  }
+
+	// Convert the String priority in the form of integer before saving it to Database
+	void updatePriorityAsInt(String value) {
+		switch (value) {
+			case 'High':
+				note.priority = 1;
+				break;
+			case 'Low':
+				note.priority = 2;
+				break;
+		}
+	}
+
+	// Convert int priority to String priority and display it to user in DropDown
+	String getPriorityAsString(int value) {
+		String priority;
+		switch (value) {
+			case 1:
+				priority = _priorities[0];  // 'High'
+				break;
+			case 2:
+				priority = _priorities[1];  // 'Low'
+				break;
+		}
+		return priority;
+	}
+
+	// Update the title of Note object
+  void updateTitle(){
+    note.title = titleController.text;
+  }
+
+	// Update the description of Note object
+	void updateDescription() {
+		note.description = descriptionController.text;
+	}
+
+	// Save data to database
+	void _save() async {
+
+		moveToLastScreen();
+
+		note.date = DateFormat.yMMMd().format(DateTime.now());
+		int result;
+		if (note.id != null) {  // Case 1: Update operation
+			result = await helper.updateNote(note);
+		} else { // Case 2: Insert Operation
+			result = await helper.insertNote(note);
+		}
+
+		if (result != 0) {  // Success
+			_showAlertDialog('Status', 'Note Saved Successfully');
+		} else {  // Failure
+			_showAlertDialog('Status', 'Problem Saving Note');
+		}
+
+	}
+
+	void _delete() async {
+
+		moveToLastScreen();
+
+		// Case 1: If user is trying to delete the NEW NOTE i.e. he has come to
+		// the detail page by pressing the FAB of NoteList page.
+		if (note.id == null) {
+			_showAlertDialog('Status', 'No Note was deleted');
+			return;
+		}
+
+		// Case 2: User is trying to delete the old note that already has a valid ID.
+		int result = await helper.deleteNote(note.id);
+		if (result != 0) {
+			_showAlertDialog('Status', 'Note Deleted Successfully');
+		} else {
+			_showAlertDialog('Status', 'Error Occured while Deleting Note');
+		}
+	}
+
+	void _showAlertDialog(String title, String message) {
+
+		AlertDialog alertDialog = AlertDialog(
+			title: Text(title),
+			content: Text(message),
+		);
+		showDialog(
+				context: context,
+				builder: (_) => alertDialog
+		);
+	}
 
 }
